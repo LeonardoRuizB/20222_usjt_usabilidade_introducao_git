4a1ac18b3f0e35ef5963d61cf3cfb2f54bcb44bb
LeonardoRuizB <leobottura12@gmail.com>
2022-08-20 23:33:14 -0300

    Incluindo arquivos faltantes do commit anterior

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,5 +1,9 @@
+import java.io.Externalizable;
+
 public class TesteEmpregado {
     public static void main(String[] args) {
-        
+        Empregado e1 = new Empregado();
+        Empregado e2 = new Empregado();
+        Empregado e3 = new Empregado();
     }
 }
